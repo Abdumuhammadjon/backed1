2d32b88afcab3234b24584edafe0ffe5d2ec7f4d
Abdumuhammadjon <nestnext551@gmail.com>
2025-08-15 11:11:55 +0500

    Update redis.js avgust

--- redis/redis.js
+++ redis/redis.js
@@ -1,11 +1,16 @@
 const Redis = require('ioredis');
 
 const redis = new Redis({
-  host: 'redis-17635.c244.us-east-1-2.ec2.redns.redis-cloud.com',
-  port: 17635,
-  password: 'fWY10h6Kefzdh8XK9wM5L5SwLbQdwEXZ',
+  host: 'redis-12319.c273.us-east-1-2.ec2.redns.redis-cloud.com',
+  port: 12319,
+  password: '4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I',
 });
 
 redis.ping()
   .then(() => console.log('✅ Redis-ga muvaffaqiyatli ulandi!'))
   .catch(err => console.error('❌ Redis xatosi:', err));
+
+
+
+
+// redis-cli -u redis:// standart : 4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I @ standart 4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I redis-12319.c273.us-east-1-2.ec2.redns.redis-cloud.com 12319
