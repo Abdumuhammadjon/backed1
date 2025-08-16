2db6c12003c35a4a41dcd99517bbb813c9925790
Abdumuhammadjon <nestnext551@gmail.com>
2025-08-16 09:01:00 +0500

    Update redisClient.js

--- redisClient.js
+++ redisClient.js
@@ -7,7 +7,7 @@ const redisClient = createClient({
     password: '4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I', 
     socket: {
         host: 'redis-12319.c273.us-east-1-2.ec2.redns.redis-cloud.com',
-        port: process.env.REDIS_PORT,
+        port:  12319,
     
     }
 });
