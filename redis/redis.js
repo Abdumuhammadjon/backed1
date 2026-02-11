8aa34655c6d5babc311e44a1079229004005d62e
Abdumuhammadjon <nestnext551@gmail.com>
2026-02-11 11:55:13 +0500

    Refactofr Redis connection error handling and add TLS
    Removed unnecessary error message text and added TLS configuration.dfgdgfdgfdgfd

--- redis/redis.js
+++ redis/redis.js
@@ -4,13 +4,10 @@ const redis = new Redis({
   host: 'redis-17763.c61.us-east-1-3.ec2.cloud.redislabs.com',
   port: 17763,
   password: 'FiqxUvBB1ZyJBPRgdALRh48NzqOrrm8Z',
+  tls: {}   // 🔥 MUHIM
 });
 
 redis.ping()
   .then(() => console.log('✅ Redis-ga muvaffaqiyatli ulandi!'))
-  .catch(err => console.error('❌ Redis xatosi:', "ishlamayapti",  err));
+  .catch(err => console.error('❌ Redis xatosi:', err));
 
-
-
-
-// redis-cli -u redis:// standart : 4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I @ standart 4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I redis-12319.c273.us-east-1-2.ec2.redns.redis-cloud.com 12319
