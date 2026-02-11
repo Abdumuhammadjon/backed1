b03b292e9e32c2131ef21c3d7419167a1012b531
Abdumuhammadjon <nestnext551@gmail.com>
2026-02-11 11:45:12 +0500

    gfdgfdgfd
    fdgdgfgfdg

--- redis/redis.js
+++ redis/redis.js
@@ -1,16 +1,16 @@
 const Redis = require('ioredis');
 
 const redis = new Redis({
-  host: 'redis-12319.c273.us-east-1-2.ec2.redns.redis-cloud.com',
-  port: 12319,
-  password: '4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I',
+  host: 'redis-17763.c61.us-east-1-3.ec2.cloud.redislabs.com',
+  port: 17763,
+  password: 'FiqxUvBB1ZyJBPRgdALRh48NzqOrrm8Z',
 });
 
 redis.ping()
   .then(() => console.log('✅ Redis-ga muvaffaqiyatli ulandi!'))
   .catch(err => console.error('❌ Redis xatosi:', "ishlamayapti",  err));
 
-
+ 
 
 
 // redis-cli -u redis:// standart : 4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I @ standart 4K3l1H41SbTDPxviB3EILSY6NPQLEQ6I redis-12319.c273.us-east-1-2.ec2.redns.redis-cloud.com 12319
