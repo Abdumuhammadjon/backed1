f3674d19d51ca2f44e7684219aa54e5aa55e10c8
Abdumuhammadjon <nestnext551@gmail.com>
2026-02-14 08:56:16 +0500

    Update auth.js
    dfedfsfs

--- Controllers/Auth/auth.js
+++ Controllers/Auth/auth.js
@@ -116,7 +116,7 @@ const { data: subject } = await supabase
   token,
   subject_id: subject ? subject.id : null,
   adminId: subject ? subject.admin : null,
-  userId: user ? user.id : null
+  userId: user.id
 });
 
   } catch (error) {
